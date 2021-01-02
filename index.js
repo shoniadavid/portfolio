66c988f748aaac3bd186e01deb4d93028facb718
David Shonia <72626283+shoniadavid@users.noreply.github.com>
2021-01-02 23:12:12 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+        setTimeout(function(){
+            $("body, html").css("overflow","visible");
+            $(".animated-text").css("display","none");
+        },
+        6000
+        );
+
+
+
+        function redirect(url){
+            window.open(url, "_blank");
+        }
+
+
+
+
+
+
+        
